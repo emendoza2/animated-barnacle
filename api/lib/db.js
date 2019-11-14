f028f22ad565f7cad58b4099af25637918790ac5
Elijah Mendoza <elijahtanchimendoza@gmail.com>
2019-11-14 11:57:44 +0800

    Unfortunately

--- api/lib/db.js
+++ api/lib/db.js
@@ -4,12 +4,11 @@ const { exec } = require('child_process');
 const database = {};
 database.init = function () {
     var db = new sqlite3.Database('data/database.db');
-    var teams = ["Backyardigans", "Wonder Peeps"];
-    
+    var teams = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8", "Team 9", "Team 10", "Team 11", "Team 12", "Team 13", "Team 14", "Team 15", "Team 16", "Team 17", "Team 18", "Team 19", "Team 20"];
     db.serialize(function () {
         db.run('DROP TABLE IF EXISTS teams');
         db.run('DROP TABLE IF EXISTS people');
-        db.run(`CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, name VARCHAR(255), teamNumber INTEGER)`);
+        db.run(`CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, name VARCHAR(255), teamNumber INTEGER, foodChoice VARCHAR(255))`);
         db.run('CREATE TABLE teams (id INTEGER PRIMARY KEY, name VARCHAR(255), memberCount INT default 0)');
         var stmt = db.prepare('INSERT INTO teams (name) VALUES (?)');
         for (let i = 0; i < teams.length; i++) {
@@ -22,14 +21,14 @@ database.init = function () {
     db.close();
 };
 
-database.erase = function() {
+database.erase = function () {
     var db = new sqlite3.Database('data/database.db');
     db.run('DROP TABLE IF EXISTS teams');
     db.run('DROP TABLE IF EXISTS people');
     db.close();
 }
 
-database.run = function(db, sql, params = null) {
+database.run = function (db, sql, params = null) {
     return new Promise((resolve, reject) => {
         let stmt = db.prepare(sql);
         stmt.run(params, (err) => {
@@ -41,7 +40,7 @@ database.run = function(db, sql, params = null) {
     });
 }
 
-database.get = function(db, sql, params) {
+database.get = function (db, sql, params) {
     return new Promise((resolve, reject) => {
         let stmt = db.prepare(sql);
         if (params) {
@@ -59,7 +58,7 @@ database.get = function(db, sql, params) {
     });
 }
 
-database.all = function(db, sql, params) {
+database.all = function (db, sql, params) {
     return new Promise((resolve, reject) => {
         let stmt = db.prepare(sql);
         if (params) {
@@ -77,7 +76,7 @@ database.all = function(db, sql, params) {
     });
 }
 
-database.export = function() {
+database.export = function () {
     exec('sqlite3 -header -csv "./data/database.db" "SELECT * FROM teams;" > ./data/exports/teams.csv');
     exec('sqlite3 -header -csv "./data/database.db" "SELECT * FROM people;" > ./data/exports/people.csv');
 }
--- api/routes/ppp.js
+++ api/routes/ppp.js
@@ -5,20 +5,46 @@ const db = new sqlite3.Database('data/database.db');
 const database = require('../lib/db');
 const path = require('path');
 const { createCanvas, loadImage } = require('canvas');
+const PDFDocument = require('pdfkit');
+
+const foodRatios = [
+    ["Pizza", 0.35],
+    ["Pasta", 0.25],
+    ["Chicken", 0.25],
+    ["Free Admission", 0],
+    ["Dessert", 0.15],
+    ["Pineapple", 0]
+];
+
+function select() {
+    let randomNumber = Math.random();
+    let i = 0;
+    while (randomNumber > foodRatios[i][1]) {
+        randomNumber -= foodRatios[i][1];
+        i++;
+    }
+    return i;
+}   
 
 //TODO: FIX PROMISES
 // A super simple api that returns people's teams and automagically assigns them teams.
 
 
-function save(personName) {
-    return new Promise(async (resolve, reject) => {
-        const row = await database.get(db, 'SELECT (id) FROM teams ORDER BY memberCount');
-        let teamId = row.id;
-        database.run(db, 'INSERT INTO people (name, teamNumber) VALUES (?, ?)', [personName, teamId]);
-        database.run(db, 'UPDATE teams SET memberCount = memberCount + 1 WHERE id = ?', [teamId]);
-        let newRow = { newPerson: true, teamNumber: teamId };
-        resolve(newRow);
-    })
+async function save(personName) {
+    try {
+        return new Promise(async (resolve, reject) => {
+            const row = await database.get(db, 'SELECT (id) FROM teams ORDER BY memberCount');
+            let teamId = row.id;
+            let foodChoice = foodRatios[select()][1];
+            database.run(db, 'INSERT INTO people (name, teamNumber, foodChoice) VALUES (?, ?, ?)', [personName, teamId, foodChoice]);
+            database.run(db, 'UPDATE teams SET memberCount = memberCount + 1 WHERE id = ?', [teamId]);
+            let newRow = { newPerson: true, teamNumber: teamId };
+            resolve(newRow);
+        });
+    }
+    catch (err) {
+        return console.log(err);
+    }
 }
 
 function sendAsPNG(response, canvas) {
@@ -58,13 +84,36 @@ router.get('/img', async function (req, res) {
     var canvas = createCanvas(200, 50);
     var context = canvas.getContext("2d");
     context.fillStyle = "#FFF";
-    context.fillRect(0, 0, 200, 150);
+    context.fillRect(0, 0, 200, 100);
     context.fillStyle = "#000";
-    context.font = "30px Helvetica";
-    context.textAlign = "center";
+    context.font = "12px Helvetica";
+    context.textAlign = "left";
     context.textBaseline = "middle";
-    context.fillText(teamName, canvas.width/2, canvas.height/2);
+    context.fillText(teamName, 10, canvas.height / 2);
     sendAsPNG(res, canvas);
+});
+
+router.get('/ticket', async function (req, res) {
+    let query = req.query;
+    let personName = query.name;
+    if (!personName) {
+        res.send(res.render('index', { title: 'Welcome to PPP!' }));
+        return;
+    }
+    let row = await database.get(db, 'SELECT teamNumber, foodChoice FROM people WHERE name = ?', [personName]);
+    if (typeof row === "undefined") {
+        row = await save(personName);
+    } else {
+        let teamNumber = row.teamNumber;
+        let foodChoice = row.foodChoice;
+    }
+    const doc = new PDFDocument;
+    doc.pipe(res);
+    doc.fontSize(48);
+    doc.font('Helvetica-Bold');
+    doc.text('Ticket for '+req.query.name);
+    
+    doc.end();
 });
 
 router.get('/person', async function (req, res) {
